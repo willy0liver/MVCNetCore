29b5cef1bcdc6dd94ee8d74f1dfe4f1d4cae956e
Willy0liver <escorpio17sc@gamil.com>
2022-02-17 18:05:38 -0500

    Primera Carga

---
+++ Controllers/AlumnoItemsController.cs
@@ -0,0 +1,107 @@
+﻿using ExamenMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ExamenMVC.Controllers
+{
+    public class AlumnoItemsController : Controller
+    {
+        HttpClientHandler _clientHandler = new HttpClientHandler();
+
+        AlumnoItem _oAlumnoItem = new AlumnoItem();
+        List<AlumnoItem> _oAlumnoItems = new List<AlumnoItem>();
+
+        public AlumnoItemsController()
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<List<AlumnoItem>> GetAllAlumnoItems()
+        {
+            _oAlumnoItems = new List<AlumnoItem>();
+
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44325/api/alumnos"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _oAlumnoItems = JsonConvert.DeserializeObject<List<AlumnoItem>>(apiResponse);
+                }
+            }
+
+            return _oAlumnoItems;
+        }
+
+        [HttpGet]
+        public async Task<AlumnoItem> GetById(int alumnoItemId)
+        {
+            _oAlumnoItem = new AlumnoItem();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44325/api/alumnos/" + alumnoItemId))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _oAlumnoItem = JsonConvert.DeserializeObject<AlumnoItem>(apiResponse);
+                }
+            }
+            return _oAlumnoItem;
+        }
+
+        [HttpPost]
+        public async Task<AlumnoItem> AddAlumnoItem(int alumnoItemId, [FromForm] AlumnoItem oAlumnoItem)
+        {
+            _oAlumnoItem = new AlumnoItem();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(oAlumnoItem), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:44325/api/alumnos?id=" + alumnoItemId, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _oAlumnoItem = JsonConvert.DeserializeObject<AlumnoItem>(apiResponse);
+                }
+            }
+            return _oAlumnoItem;
+        }
+
+        [HttpPut]
+        public async void updateAlumnoItem(int alumnoItemId, [FromForm] AlumnoItem oAlumnoItem)
+        {
+            _oAlumnoItem = new AlumnoItem();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(oAlumnoItem), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PutAsync("https://localhost:44325/api/alumnos?id=" + alumnoItemId, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    //_oAlumnoItem = JsonConvert.DeserializeObject<AlumnoItem>(apiResponse);
+                }
+            }
+        }
+
+        [HttpDelete]
+        public async Task<string> Delete(int alumnoItemId)
+        {
+            string message = "";
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.DeleteAsync("https://localhost:44325/api/alumnos/" + alumnoItemId))
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return message;
+        }
+    }
+}
---
+++ Models/AlumnoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenMVC.Models
+{
+    public class AlumnoItem
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Status { get; set; }
+    }
+}
